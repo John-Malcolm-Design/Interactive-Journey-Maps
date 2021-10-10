cfb9674b8e0c0e57eeedc44a13b36b739d36c07a
John Malcolm Anderson <hi@johnmalcolmdesign.com>
2021-10-11 01:47:37 +0300

    removing hover effect

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ import './Header.css';
 const Header = () => (
   <header>
     <div>
-        <h1>🍥 Access Finland Co-design Workshop</h1>
+        <h1>🍥 Access Finland: Co-design Workshop</h1>
         <h2>International students don’t have the same work opportunities as local students</h2>
     </div>
 </header>
